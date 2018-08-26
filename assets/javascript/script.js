7913ed5180cf290b9cac7bb0d7dd2d37a5143bee
Ajita Srivastava <ajitas4000@gmail.com>
2018-08-26 16:18:45 -0700

    editing style

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -64,7 +64,7 @@ $(document).ready(function(){
             var projectLineDiv = $("<div>");
             projectLineDiv.addClass("project-line");
             var projectDiv = $("<div>");
-            projectDiv.addClass("card project-div");
+            projectDiv.addClass("project-div");
             projectDiv.append(`<div class="row">
                                 <div class="col-md-12 project-image-area">
                                    <img class="img-fluid project-image" alt=`+projects[i].name+` src=`+projects[i].image+`>
@@ -96,31 +96,31 @@ $(document).ready(function(){
         for(var i =0;i<projects.length ;i++){
             if(projects[i].topics.indexOf(tag) !== -1){
                 var projectLineDiv = $("<div>");
-                projectLineDiv.addClass("project-line");
-                var projectDiv = $("<div>");
-                projectDiv.addClass("card project-div");
-                projectDiv.append(`<div class="row">
-                                    <div class="col-md-12 project-image-area">
-                                    <img class="img-fluid project-image" alt=`+projects[i].name+` src=`+projects[i].image+`>
+            projectLineDiv.addClass("project-line");
+            var projectDiv = $("<div>");
+            projectDiv.addClass("project-div");
+            projectDiv.append(`<div class="row">
+                                <div class="col-md-12 project-image-area">
+                                   <img class="img-fluid project-image" alt=`+projects[i].name+` src=`+projects[i].image+`>
+                                </div>
+                            </div>`);
+            var projectDiv1 = $("<div>");
+            projectDiv1.append(`<div class="row">
+                                    <div class="col-md-9"></div>
+                                    <div class="col-md-3 code-link">
+                                        <a href=`+projects[i].deployedLink+` target="_blank"><img class="img-fluid project-icon" src="assets/images/play-icon.png"></a>
                                     </div>
                                 </div>`);
-                var projectDiv1 = $("<div>");
-                projectDiv1.append(`<div class="row">
-                                        <div class="col-md-9"></div>
-                                        <div class="col-md-3 code-link">
-                                            <a href=`+projects[i].deployedLink+` target="_blank"><img class="img-fluid project-icon" src="assets/images/play-icon.png"></a>
-                                        </div>
-                                    </div>`);
-                projectDiv1.append(`<div class="row">
-                                        <div class="col-md-9"></div>
-                                        <div class="col-md-3 code-link">
-                                            <a href=`+projects[i].codeLink+` target="_blank"><img class="img-fluid project-icon" src="assets/images/github-icon.png"></a>
-                                        </div>
-                                    </div>`);
-                projectDiv1.addClass("card project-div1");
-                projectLineDiv.append(projectDiv1);
-                projectLineDiv.append(projectDiv);
-                $("#project-area").append(projectLineDiv);
+            projectDiv1.append(`<div class="row">
+                                    <div class="col-md-9"></div>
+                                    <div class="col-md-3 code-link">
+                                        <a href=`+projects[i].codeLink+` target="_blank"><img class="img-fluid project-icon" src="assets/images/github-icon.png"></a>
+                                    </div>
+                                </div>`);
+            projectDiv1.addClass("card project-div1");
+            projectLineDiv.append(projectDiv1);
+            projectLineDiv.append(projectDiv);
+            $("#project-area").append(projectLineDiv);
             }
         }
     }
