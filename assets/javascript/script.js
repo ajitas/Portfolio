40725f30b2005b5c95650e88f0010e58536611f9
Ajita Srivastava <ajitas4000@gmail.com>
2018-08-29 21:55:49 -0700

    editing project images

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,30 +1,30 @@
 $(document).ready(function(){
 
-    var topicTags = ["HTML","CSS","BootStrap","Javascript","jQuery","APIs","AJAX","Firebase","Moment.js","Just for Fun"];
+    var topicTags = ["HTML","CSS","BootStrap","Javascript","jQuery","APIs","AJAX","Firebase","Moment.js","Node.js","Just for Fun"];
     var projects = [{
                         name:"Crystal Game",
                         topics:["HTML","CSS","Javascript","jQuery","Just for Fun"],
-                        image:"assets/images/crystal-game.jpg",
+                        image:"assets/images/crystal-game.png",
                         deployedLink:"https://ajitas.github.io/Crystal-Game/",
                         codeLink:"https://github.com/ajitas/Crystal-Game"
                     },
                     {
                         name:"Word Guess Game",
                         topics:["HTML","CSS","Javascript","Just for Fun"],
-                        image:"assets/images/word-guess-game.jpg",
+                        image:"assets/images/word-guess-game.png",
                         deployedLink:"https://ajitas.github.io/Word-Guess-Game/",
                         codeLink:"https://github.com/ajitas/Word-Guess-Game"
                     },
                     {
                         name:"Pokemon Combat Game",
                         topics:["HTML","CSS","Javascript","jQuery","Just for Fun"],
-                        image:"assets/images/pokemon-combat-game.jpg",
+                        image:"assets/images/pokemon-combat-game.png",
                         deployedLink:"https://ajitas.github.io/Pokemon-Combat-Game/",
                         codeLink:"https://github.com/ajitas/Pokemon-Combat-Game"
                     },
                     {
                         name:"Trivia Timed Game",
-                        topics:["HTML","CSS","BootStrap","Javascript","jQuery","Just for Fun"],
+                        topics:["HTML","CSS","BootStrap","Javascript","jQuery"],
                         image:"assets/images/trivia-timed.png",
                         deployedLink:"https://ajitas.github.io/Trivia-Timed/",
                         codeLink:"https://github.com/ajitas/Trivia-Timed"
@@ -56,8 +56,22 @@ $(document).ready(function(){
                         image:"assets/images/lyriquiz.png",
                         deployedLink:"https://ajitas.github.io/LyriQuiz/",
                         codeLink:"https://github.com/ajitas/LyriQuiz"
+                    },
+                    {
+                        name:"LIRIBot-node",
+                        topics:["Javascript","Node.js","APIs","Moment.js"],
+                        image:"assets/images/liri.gif",
+                        deployedLink:"#",
+                        codeLink:"https://github.com/ajitas/liri-node-app"
+                    },
+                    {
+                        name:"Hangman-node",
+                        topics:["Javascript","Node.js"],
+                        image:"assets/images/hangman-correct.gif",
+                        deployedLink:"#",
+                        codeLink:"https://github.com/ajitas/Word-Guess-Game-CLI"
                     }
-                    ]
+                    ];
 
     function showTopicTagButtons(){
         $("#button-area").empty();
@@ -80,28 +94,44 @@ $(document).ready(function(){
             projectLineDiv.addClass("project-line");
             var projectDiv = $("<div>");
             projectDiv.addClass("project-div");
-            projectDiv.append(`<div class="row">
-                                <div class="col-md-12 project-image-area">
-                                   <img class="img-fluid project-image" alt=`+projects[i].name+` src=`+projects[i].image+`>
+            projectDiv.append(` <div class="row">
+                                    <div class="col-md-12 text-center project-heading">`+
+                                    projects[i].name+
+                                    `</div>
                                 </div>
-                            </div>`);
-            var projectDiv1 = $("<div>");
-            projectDiv1.append(`<div class="row">
-                                    <div class="col-md-9"></div>
-                                    <div class="col-md-3 code-link">
-                                        <a href=`+projects[i].deployedLink+` target="_blank"><img class="img-fluid project-icon" src="assets/images/play-icon.png"></a>
+                                <div class="row">
+                                    <div class="col-md-12 project-image-area">
+                                    <img class="img-fluid project-image" alt=`+projects[i].name+` src=`+projects[i].image+`>
                                     </div>
                                 </div>`);
+            var projectDiv1 = $("<div>");
+            if(projects[i].deployedLink === "#"){
+                projectDiv1.append(`<div class="row">
+                                        <div class="col-md-9"></div>
+                                        <div class="col-md-3 code-link">
+                                           <img class="img-fluid project-icon" id="link-image-button-disabled" src="assets/images/play-icon.png">
+                                        </div>
+                                    </div>`);
+            }
+            else{
+                projectDiv1.append(`<div class="row">
+                                        <div class="col-md-9"></div>
+                                        <div class="col-md-3 code-link">
+                                            <a href=`+projects[i].deployedLink+` target="_blank"><img class="img-fluid project-icon" src="assets/images/play-icon.png"></a>
+                                        </div>
+                                    </div>`);
+            }
             projectDiv1.append(`<div class="row">
                                     <div class="col-md-9"></div>
                                     <div class="col-md-3 code-link">
-                                        <a href=`+projects[i].codeLink+` target="_blank"><img class="img-fluid project-icon" src="assets/images/github-icon.png"></a>
+                                        <a href=`+projects[i].codeLink+` target="_blank"><img class="img-fluid project-icon" id="code-image-button" src="assets/images/github-icon.png"></a>
                                     </div>
                                 </div>`);
             projectDiv1.addClass("card project-div1");
             projectLineDiv.append(projectDiv1);
             projectLineDiv.append(projectDiv);
             $("#project-area").append(projectLineDiv);
+            
         }
 
     }
@@ -120,12 +150,22 @@ $(document).ready(function(){
                                 </div>
                             </div>`);
             var projectDiv1 = $("<div>");
-            projectDiv1.append(`<div class="row">
-                                    <div class="col-md-9"></div>
-                                    <div class="col-md-3 code-link">
-                                        <a href=`+projects[i].deployedLink+` target="_blank"><img class="img-fluid project-icon" src="assets/images/play-icon.png"></a>
-                                    </div>
-                                </div>`);
+            if(projects[i].deployedLink === "#"){
+                projectDiv1.append(`<div class="row">
+                                        <div class="col-md-9"></div>
+                                        <div class="col-md-3 code-link">
+                                           <img class="img-fluid project-icon" id="link-image-button-disabled" src="assets/images/play-icon.png">
+                                        </div>
+                                    </div>`);
+            }
+            else{
+                projectDiv1.append(`<div class="row">
+                                        <div class="col-md-9"></div>
+                                        <div class="col-md-3 code-link">
+                                            <a href=`+projects[i].deployedLink+` target="_blank"><img class="img-fluid project-icon" src="assets/images/play-icon.png"></a>
+                                        </div>
+                                    </div>`);
+            }
             projectDiv1.append(`<div class="row">
                                     <div class="col-md-9"></div>
                                     <div class="col-md-3 code-link">
